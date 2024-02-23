5ea0467dac682906ed1343f5639895b4add3aa2a
zamroni <forheron@gmail.com>
2024-02-24 00:43:29 +0700

    change filter

--- src/app/components/SortGeneral.js
+++ src/app/components/SortGeneral.js
@@ -1,38 +1,46 @@
-"use client"
+"use client";
 
 import React, { useState, useRef, useEffect } from "react";
 
-const options = ["Relevance", "Most Popular", "Top Ranked", "Top Rated", "Newest", "Release Date", "Recently Added"];
+const options = [
+  "Relevance",
+  "Most Popular",
+  "Top Ranked",
+  "Top Rated",
+  "Newest",
+  "Release Date",
+  "Recently Added",
+];
 const sorts = [
-    {
-      name: "Relevance",
-      slug: "Relevance",
-    },
-    {
-      name: "Most Popular",
-      slug: "Popular",
-    },
-    {
-      name: "Top Ranked",
-      slug: "Top",
-    },
-    {
-      name: "Top Rated",
-      slug: "Rated",
-    },
-    {
-      name: "Newest",
-      slug: "Newest",
-    },
-    {
-      name: "Release Date",
-      slug: "Release",
-    },
-    {
-      name: "Recently Added",
-      slug: "Recently",
-    },
-  ]
+  {
+    name: "Relevance",
+    slug: "Relevance",
+  },
+  {
+    name: "Most Popular",
+    slug: "Popular",
+  },
+  {
+    name: "Top Ranked",
+    slug: "Top",
+  },
+  {
+    name: "Top Rated",
+    slug: "Rated",
+  },
+  {
+    name: "Newest",
+    slug: "Newest",
+  },
+  {
+    name: "Release Date",
+    slug: "Release",
+  },
+  {
+    name: "Recently Added",
+    slug: "Recently",
+  },
+];
 
 const SortGeneral = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -64,46 +72,54 @@ const SortGeneral = () => {
 
   return (
     <>
-    <div className="flex  z-30">
-    <div className="relative flex items-center justify-between px-2 border border-[#0c9bff] bg-transparent text-white rounded-full w-40 h-8 cursor-pointer" ref={autoRef} onClick={toggling}>
-          <button className="text-[11px]  font-semibold uppercase pl-2 text-[#0c9bff]">
-            {selectedOption || "Relevance"}
+      <div className="flex  z-30">
+        <div
+          className={`relative flex items-center justify-between px-0 border border-[#cccdce] ${
+            isOpen
+              ? "bg-white text-black"
+              : "bg-transparent text-[#ddd] hover:bg-[#31363b]"
+          }  rounded min-w-52 h-9 cursor-pointer`}
+          ref={autoRef}
+          onClick={toggling}
+        >
+          <button className="text-[13px]  font-bold uppercase pl-2 px-2 tracking-wide">
+            sort by {selectedOption || "Relevance"}
           </button>
-          <span >
-           
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="currentColor"
-                className={`w-3 h-3 ${isOpen? 'rotate-180' : ''} text-[#0c9bff]`}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                />
-              </svg>
-            
+          <span className="px-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={3}
+              stroke="currentColor"
+              className={`w-4 h-4 ${
+                isOpen ? "rotate-180 text-black" : "text-[#ddd]"
+              } `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
           </span>
           {isOpen && (
-            <div className="min-w-[100px] absolute top-5 py-2 right-0 z-10 mt-4 origin-top-right rounded border border-gray-200 bg-white shadow-lg">
+            <div className="min-w-52 absolute top-6 py-2 right-0 z-10 mt-4 origin-top-right rounded  bg-[#474645]">
               {options.map((option, index) => (
-                <div className="px-2" key={index}>
+                <div className="px-0" key={index}>
                   <button
-                    type="button"
+                    // type="button"
                     onClick={onOptionClicked(option)}
-                    className="flex py-1 px-2 w-full uppercase rounded text-[11px] text-gray-500 no-underline hover:bg-blue-600 hover:text-white"
+                    className="flex py-1.5 px-3 w-full uppercase font-semibold text-[13px] text-[#d8d9d9] no-underline hover:bg-[#65686a] hover:text-white"
                   >
-                    <span>{option}</span>
+                    <span className="">{option}</span>
                   </button>
                 </div>
               ))}
             </div>
           )}
         </div>
-        </div>
+      </div>
     </>
   );
 };
--- src/app/components/SortStatus.js
+++ src/app/components/SortStatus.js
@@ -48,46 +48,54 @@ const SortStatus = () => {
 
   return (
     <>
-    <div className="relative2 block z-30">
-        <div className="relative flex items-center justify-between px-2 border border-[#0c9bff] bg-transparent text-white rounded-full w-28 h-8 cursor-pointer" ref={autoRef} onClick={toggling}>
-          <button className="text-[11px]  font-semibold uppercase pl-2 text-[#0c9bff]">
+    <div className="flex  z-30">
+        <div
+          className={`relative flex items-center justify-between px-0 border border-[#cccdce] ${
+            isOpen
+              ? "bg-white text-black"
+              : "bg-transparent text-[#ddd] hover:bg-[#31363b]"
+          }  rounded min-w-36 h-9 cursor-pointer`}
+          ref={autoRef}
+          onClick={toggling}
+        >
+          <button className="text-[13px]  font-bold uppercase pl-2 px-2 tracking-wide">
             {selectedOption || "Completed"}
           </button>
-          <span >
-           
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="currentColor"
-                className={`w-3 h-3 ${isOpen? 'rotate-180' : ''} text-[#0c9bff]`}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
-                />
-              </svg>
-            
+          <span className="px-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={3}
+              stroke="currentColor"
+              className={`w-4 h-4 ${
+                isOpen ? "rotate-180 text-black" : "text-[#ddd]"
+              } `}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m19.5 8.25-7.5 7.5-7.5-7.5"
+              />
+            </svg>
           </span>
           {isOpen && (
-            <div className="min-w-[100px] absolute top-5 py-2 right-0 z-10 mt-4 origin-top-right rounded border border- bg-[#474645] shadow-lg">
+            <div className="min-w-36 absolute top-6 py-2 right-0 z-10 mt-4 origin-top-right rounded  bg-[#474645]">
               {options.map((option, index) => (
-                <div className="px-2" key={index}>
+                <div className="px-0" key={index}>
                   <button
                     // type="button"
                     onClick={onOptionClicked(option)}
-                    className="flex py-1 px-2 w-full uppercase rounded text-[11px] text-[#d8d9d9] no-underline hover:bg-blue-600 hover:text-white"
+                    className="flex py-1.5 px-3 w-full uppercase font-semibold text-[13px] text-[#d8d9d9] no-underline hover:bg-[#65686a] hover:text-white"
                   >
-                    <span>{option}</span>
+                    <span className="">{option}</span>
                   </button>
                 </div>
               ))}
             </div>
           )}
         </div>
-        </div>
+      </div>
     </>
   );
 };
